6eef8039a1e511b33a2130bdc42cdb58fea19730
thaidinh <thai562003@gmail.com>
2024-06-30 03:28:33 +0700

    fix service

--- src/services/libraryService.js
+++ src/services/libraryService.js
@@ -3,7 +3,7 @@ import httpRequest from '~/utils/httpRequest';
 export const getProjects = async () => {
     try {
         const response = await httpRequest.get('/videos');
-        return response.data.data.data;
+        return response.data.data;
     } catch (error) {
         console.error('Error fetching products', error);
         throw error;
