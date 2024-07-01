ee5b60e064045fd035081c87082e7a7bfb1ec3ed
thaidinh <thai562003@gmail.com>
2024-07-01 13:28:09 +0700

    Feat/Loading-Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import { DefaultLayout } from '~/layouts';
+import LoadingScreen from './components/LoadingScreen';
 
 function App() {
+    const [isLoading, setIsLoading] = useState(true); // State để quản lý hiển thị Loading
+
+    // Simulate loading delay
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false); // Tắt màn hình Loading sau 2 giây (simulated)
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (isLoading) {
+        return <LoadingScreen />; // Hiển thị màn hình Loading khi đang load dữ liệu
+    }
+
     return (
         <Router>
             <div className="App">
---
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './LoadingScreen.module.scss';
+
+const LoadingScreen = () => {
+    return (
+        <div className={styles.loading}>
+            <div className={styles.spinnerContainer}>
+                <div className={styles.dot1}></div>
+                <div className={styles.dot2}></div>
+                <div className={styles.dot3}></div>
+            </div>
+            <div className={styles.loadingText}>Loading</div>
+        </div>
+    );
+};
+
+export default LoadingScreen;
