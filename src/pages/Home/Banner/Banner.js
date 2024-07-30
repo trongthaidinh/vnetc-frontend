041deab66c58d37dbe444efaa3c9759f118f4ff3
thaidinh <thai562003@gmail.com>
2024-07-31 00:58:44 +0700

    update banner

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,13 +1,20 @@
-import React, { useEffect, useRef, useState, useCallback } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
-import styles from './Banner.module.scss';
+import React, { useEffect, useState, useCallback } from 'react';
 import { getConfiguration } from '~/services/configurationService';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import styles from './Banner.module.scss';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
 
 const Banner = () => {
-    const [currentSlide, setCurrentSlide] = useState(0);
     const [slides, setSlides] = useState([]);
-    const slideIntervalRef = useRef(null);
 
     const fetchSlides = useCallback(async () => {
         try {
@@ -19,75 +26,54 @@ const Banner = () => {
         }
     }, []);
 
-    const nextSlide = useCallback(() => {
-        setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
-    }, [slides.length]);
-
-    const prevSlide = useCallback(() => {
-        setCurrentSlide((prevSlide) => (prevSlide - 1 + slides.length) % slides.length);
-    }, [slides.length]);
-
-    const startSlideInterval = useCallback(() => {
-        slideIntervalRef.current = setInterval(() => {
-            nextSlide();
-        }, 5000);
-    }, [nextSlide]);
-
-    const stopSlideInterval = useCallback(() => {
-        if (slideIntervalRef.current) {
-            clearInterval(slideIntervalRef.current);
-        }
-    }, []);
-
-    const goToSlide = (index) => {
-        setCurrentSlide(index);
-    };
-
     useEffect(() => {
         fetchSlides();
     }, [fetchSlides]);
-
-    useEffect(() => {
-        if (slides.length > 0) {
-            startSlideInterval();
-            return () => {
-                stopSlideInterval();
-            };
-        }
-    }, [slides, startSlideInterval, stopSlideInterval]);
 
     if (slides.length === 0) {
         return null;
     }
 
     return (
-        <div className={styles.banner}>
-            <div className={styles.slidesContainer} style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
-                {slides.map((slide, index) => (
-                    <div key={index} className={styles.slide}>
-                        <img src={slide.image_url} alt={slide.title} className={styles.image} />
-                        <div className={`${styles.contentContainer} ${styles[slide.position]}`}>
-                            <div className={styles.textWrapper}>
-                                <span className={styles.text}>{slide.title}</span>
+        <div className={cx('banner')}>
+            <div className={cx('inner')}>
+                <Swiper
+                    spaceBetween={0}
+                    slidesPerView={1}
+                    loop={true}
+                    modules={[Autoplay, Navigation]}
+                    autoplay={{
+                        delay: 5000,
+                        disableOnInteraction: false,
+                    }}
+                    speed={1000}
+                    navigation={{
+                        nextEl: `.${cx('swiper-button-next')}`,
+                        prevEl: `.${cx('swiper-button-prev')}`,
+                    }}
+                    observer={true}
+                    observeParents={true}
+                    className={cx('swiper')}
+                >
+                    {slides.map((slide, index) => (
+                        <SwiperSlide key={index}>
+                            <div className={cx('imageCard')}>
+                                <img src={slide.image_url} alt={slide.title} className={cx('image')} />
+                                <div className={cx('contentContainer', slide.position)}>
+                                    <div className={cx('textWrapper')}>
+                                        <span className={cx('text')}>{slide.title}</span>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        </SwiperSlide>
+                    ))}
+                    <div className={cx('swiper-button-prev')}>
+                        <FontAwesomeIcon icon={faChevronLeft} className={cx('swiper-icon')} />
                     </div>
-                ))}
-            </div>
-            <div className={`${styles.arrow} ${styles.prev}`} onClick={prevSlide}>
-                <FontAwesomeIcon icon={faChevronLeft} />
-            </div>
-            <div className={`${styles.arrow} ${styles.next}`} onClick={nextSlide}>
-                <FontAwesomeIcon icon={faChevronRight} />
-            </div>
-            <div className={styles.pagination}>
-                {slides.map((_, index) => (
-                    <span
-                        key={index}
-                        className={`${styles.dot} ${currentSlide === index ? styles.active : ''}`}
-                        onClick={() => goToSlide(index)}
-                    />
-                ))}
+                    <div className={cx('swiper-button-next')}>
+                        <FontAwesomeIcon icon={faChevronRight} className={cx('swiper-icon')} />
+                    </div>
+                </Swiper>
             </div>
         </div>
     );
