1a2579f0cce5969a3d2e0678b7c661842cda314d
thaidinh <thai562003@gmail.com>
2024-07-31 12:47:34 +0700

    update

--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
-import styles from './Services.module.scss';
 import ServiceItem from './ServiceItem';
 import { getServices } from '~/services/serviceService';
 import { getCategoriesByType } from '~/services/categoryService';
@@ -8,6 +7,11 @@ import Title from '~/components/Title';
 import PushNotification from '~/components/PushNotification';
 import LoadingScreen from '~/components/LoadingScreen';
 import routes from '~/config/routes';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import styles from './Services.module.scss';
 
 const cx = classNames.bind(styles);
 
@@ -58,15 +62,33 @@ const Services = () => {
         <div className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <Title text="Dịch vụ" showSeeAll={true} slug={routes.services} />
-                <div className={cx('service-list')}>
-                    {categoryToServiceMap.map((category) => (
-                        <ServiceItem
-                            key={category._id}
-                            image={category.services[0].image}
-                            name={category.name}
-                            slug={category.slug}
-                        />
-                    ))}
+                <div className={cx('slide-wrapper')}>
+                    <Swiper
+                        spaceBetween={20}
+                        slidesPerView={4}
+                        breakpoints={{
+                            1280: { slidesPerView: 4 },
+                            1024: { slidesPerView: 3 },
+                            768: { slidesPerView: 2 },
+                            0: { slidesPerView: 1 },
+                        }}
+                        loop={true}
+                        modules={[Autoplay]}
+                        autoplay={{
+                            delay: 2000,
+                            disableOnInteraction: false,
+                        }}
+                    >
+                        {categoryToServiceMap.map((category) => (
+                            <SwiperSlide key={category._id} className={cx('service-slide')}>
+                                <ServiceItem
+                                    image={category.services[0].image}
+                                    name={category.name}
+                                    slug={category.slug}
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
                 </div>
             </div>
         </div>
