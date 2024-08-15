a8f1c3f965c069057bf008db5525aa2bcb459d02
thaidinh <thai562003@gmail.com>
2024-08-15 23:37:44 +0700

    Update Loading Screen

--- src/components/LoadingScreen/LoadingScreen.js
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import styles from './LoadingScreen.module.scss';
+import images from '~/assets/images';
 
 const LoadingScreen = () => {
     return (
         <div className={styles.loading}>
-            <div className={styles.spinnerContainer}>
-                <div className={styles.dot1}></div>
-                <div className={styles.dot2}></div>
-                <div className={styles.dot3}></div>
+            <img src={images.logo} alt="Logo" className={styles.logo} />
+            <div className={styles.loadingWrapper}>
+                <div className={styles.spinnerContainer}>
+                    <div className={styles.dot1}></div>
+                    <div className={styles.dot2}></div>
+                    <div className={styles.dot3}></div>
+                </div>
+                <div className={styles.loadingText}>Đang tải</div>
             </div>
-            <div className={styles.loadingText}>Đang tải</div>
         </div>
     );
 };
