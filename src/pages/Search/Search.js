bd58c6769851bf6b19f06bb18258aeadc0230122
thaidinh <thai562003@gmail.com>
2024-08-23 22:39:56 +0700

    Update Search

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -3,7 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import Card from '~/components/CardContent';
 import SuggestCard from '~/components/SuggestCard';
-import { getSearchNews, getSearchProduct, getSearchProject, getSearchService } from '~/services/searchService';
+import { getSearchNews, getSearchProject, getSearchService } from '~/services/searchService';
 import styles from './Search.module.scss';
 import ButtonGroup from '~/components/ButtonGroup';
 import PushNotification from '~/components/PushNotification';
@@ -13,7 +13,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { getCategories, getCategoriesByType } from '~/services/categoryService';
 import Title from '~/components/Title';
-import Product from '~/components/Product';
 import { getNewsPagination } from '~/services/newsService';
 import { Empty } from 'antd';
 
@@ -22,74 +21,58 @@ const cx = classNames.bind(styles);
 const Search = () => {
     const [searchResults, setSearchResults] = useState({
         news: [],
-        products: [],
         projects: [],
         services: [],
     });
 
     const [suggests, setSuggests] = useState([]);
     const [categories, setCategories] = useState([]);
-    const [categoriesService, setCategoriesService] = useState([]);
     const [categoriesProject, setCategoriesProject] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [selectedSuggestion, setSelectedSuggestion] = useState(0);
     const [currentPages, setCurrentPages] = useState({
         news: 1,
-        products: 1,
         projects: 1,
         services: 1,
     });
     const [totalPages, setTotalPages] = useState({
         news: 1,
-        products: 1,
         projects: 1,
         services: 1,
     });
     const location = useLocation();
 
     const query = new URLSearchParams(location.search).get('q');
-    const resultsPerPage = 9;
+    const resultsPerPage = 6;
 
     useEffect(() => {
         const fetchSearchResults = async () => {
             try {
                 setLoading(true);
 
-                const [
-                    suggestData,
-                    newsData,
-                    productData,
-                    projectData,
-                    serviceData,
-                    categoryData,
-                    categoryService,
-                    categoryProject,
-                ] = await Promise.all([
-                    getNewsPagination(),
-                    getSearchNews(query, resultsPerPage, currentPages.news),
-                    getSearchProduct(query, resultsPerPage, currentPages.products),
-                    getSearchProject(query, resultsPerPage, currentPages.projects),
-                    getSearchService(query, resultsPerPage, currentPages.services),
-                    getCategories(),
-                    getCategoriesByType(3),
-                    getCategoriesByType(4),
-                ]);
+                const [suggestData, newsData, projectData, serviceData, categoryData, categoryProject] =
+                    await Promise.all([
+                        getNewsPagination(1, 20),
+                        getSearchNews(query, resultsPerPage, currentPages.news),
+                        getSearchProject(query, resultsPerPage, currentPages.projects),
+                        getSearchService(query, resultsPerPage, currentPages.services),
+                        getCategories(),
+                        getCategoriesByType(3),
+                        getCategoriesByType(4),
+                    ]);
 
                 setSearchResults({
                     news: newsData.results,
-                    products: productData.products,
                     projects: projectData.results,
                     services: serviceData.results,
                 });
 
-                setSuggests(suggestData);
+                setSuggests(suggestData.news);
                 setCategories(categoryData);
-                setCategoriesService(categoryService);
                 setCategoriesProject(categoryProject);
                 setTotalPages({
                     news: newsData.totalPages,
-                    products: productData.totalPages,
                     projects: projectData.totalPages,
                     services: serviceData.totalPages,
                 });
@@ -111,18 +94,8 @@ const Search = () => {
         return category ? category.slug : '';
     };
 
-    const getProductCategorySlug = (item) => {
-        const category = categories.find((cat) => cat._id === item.category_id);
-        return category ? category.slug : '';
-    };
-
     const getProjectCategorySlug = (item) => {
         const category = categoriesProject.find((cat, index) => index === item.projectType);
-        return category ? category.slug : '';
-    };
-
-    const getServiceCategorySlug = (item) => {
-        const category = categoriesService.find((cat, index) => index === item.serviceType);
         return category ? category.slug : '';
     };
 
@@ -195,10 +168,7 @@ const Search = () => {
 
     const renderSearchResults = () => {
         const hasResults =
-            searchResults.news.length > 0 ||
-            searchResults.products.length > 0 ||
-            searchResults.projects.length > 0 ||
-            searchResults.services.length > 0;
+            searchResults.news.length > 0 || searchResults.projects.length > 0 || searchResults.services.length > 0;
 
         if (!hasResults) {
             return (
@@ -234,20 +204,6 @@ const Search = () => {
                     </>
                 )}
 
-                {searchResults.products && searchResults.products.length > 0 && (
-                    <>
-                        <Title text="Sản phẩm" />
-                        <div className={cx('product-result')}>
-                            {searchResults.products.map((item, index) => (
-                                <Link key={index} to={`${routes.products}/${getProductCategorySlug(item)}/${item.id}`}>
-                                    <Product name={item.name} image={item.image} />
-                                </Link>
-                            ))}
-                        </div>
-                        {renderPagination('products')}
-                    </>
-                )}
-
                 {searchResults.projects && searchResults.projects.length > 0 && (
                     <>
                         <Title text="Dự án" />
@@ -270,10 +226,10 @@ const Search = () => {
 
                 {searchResults.services && searchResults.services.length > 0 && (
                     <>
-                        <Title text="Dịch vụ" />
+                        <Title text="Dịch vụ - Sản Phẩm" />
                         <div className={cx('service-result')}>
                             {searchResults.services.map((item, index) => (
-                                <Link key={index} to={`${routes.services}/${getServiceCategorySlug(item)}/${item._id}`}>
+                                <Link key={index} to={`${routes.services}/${getCategorySlug(item)}/${item._id}`}>
                                     <Card
                                         title={item.name}
                                         summary={item.summary}
@@ -307,6 +263,7 @@ const Search = () => {
                                 <SuggestCard
                                     key={index}
                                     title={item.title}
+                                    summary={item.summary}
                                     image={item.images}
                                     createdAt={new Date(item.createdAt).getTime()}
                                 />
